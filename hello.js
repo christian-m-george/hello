314a317137d71a2e21367060cac8774d76e78b4f
christian george <christian.george360@gmail.com>
2021-08-11 09:30:23 -0400

    simplified

--- hello.js
+++ hello.js
@@ -1,14 +1,10 @@
 import fs from "fs";
 import { argv } from "process";
 
-const getProcessArg = () => {
-  return argv[2];
-};
-
 const writeHello = (input) => {
   fs.appendFile(`${input}`, "\nHello World", function (err) {
     if (err) console.log("error", err);
   });
 };
 
-writeHello(getProcessArg());
+writeHello(argv[2]);
